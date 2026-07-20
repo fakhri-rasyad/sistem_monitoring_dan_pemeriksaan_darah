a7f13933436e0bc348555082c8d4ce8f33729231
fakhri-rasyad <kaizerd23@gmail.com>
2026-07-20 18:24:36 +0800

    feat: Created submission dto

--- backend/dto/submission.go
+++ backend/dto/submission.go
@@ -1,4 +1,11 @@
 package dto
 
 type SubmissionCreate struct {
+	Pasien           PasienReference         `json:"pasien"`
+	AlergiPasiens    []AlergiPasienCreate    `json:"alergi_pasiens"`
+	PantanganPasiens []PantanganPasienCreate `json:"pantangan_pasiens"`
+	Kunjungan        KunjunganCreate         `json:"kunjungan"`
+	KomposisiTubuh   KomposisiTubuhCreate    `json:"komposisi_tubuh"`
+	DataLabs         []DataLabCreate         `json:"data_labs"`
+	Pemeriksaan      PemeriksaanCreate       `json:"pemeriksaan"`
 }
